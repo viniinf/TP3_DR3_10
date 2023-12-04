82c7a553d323f9fed910a5df9d51afaab4bdf4c7
vinicius.scarvalho@al.edu.infnet.com.br <vinicius.scarvalho@al.edu.infnet.com.br>
2023-12-04 14:13:35 -0300

    Q10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,39 @@
-package org.example;public class Main {
+package org.example;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            String jsonInputString = "{\"userId\": 1,"
+                    + "\"id\": 1,"
+                    + "\"title\": \"delectus aut autem\","
+                    + "\"completed\": false}";
+
+            byte[] jsonBytes = jsonInputString.getBytes(StandardCharsets.UTF_8);
+
+            try (OutputStream os = connection.getOutputStream()) {
+                os.write(jsonBytes, 0, jsonBytes.length);
+            }
+
+            int responseCode = connection.getResponseCode();
+            //Questao 10
+            System.out.println("Código de resposta: " + responseCode);
+
+            connection.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
